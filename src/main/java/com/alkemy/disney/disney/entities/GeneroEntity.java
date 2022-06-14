b02a0882f0e283768958f782b37b7cfde8a3f6e4
larrydaguilera <larrydaguilera@gmail.com>
2022-06-14 17:58:45 -0300

    Entities and relations successfuly created

---
+++ src/main/java/com/alkemy/disney/disney/entities/GeneroEntity.java
@@ -0,0 +1,28 @@
+package com.alkemy.disney.disney.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GeneratorType;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "genero")
+@Getter
+@Setter
+
+public class GeneroEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String nombre;
+
+    private String imagen;
+
+
+}
